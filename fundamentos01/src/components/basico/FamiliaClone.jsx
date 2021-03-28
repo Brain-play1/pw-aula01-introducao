5268f055f163db587bccd300c38c78ac8f0a0d31
Brain-play1 <leonardo.dinizbassiilio@gmail.com>
2021-03-28 16:02:37 +0000

    Aula 02 - Commit 03
    Signed-off-by: Brain-play1 <leonardo.dinizbassiilio@gmail.com>

---
+++ fundamentos01/src/components/basico/FamiliaClone.jsx
@@ -0,0 +1,17 @@
+import React, {cloneElement} from 'react';
+
+
+export default props => {
+
+    return(
+        <div>
+            {/*Para um unico elemento filho */}
+            {/* {cloneElement(props.children, {...props})} */}
+
+            {/*Para varios elementos filho */}
+            {props.children.map((child) => {
+                return cloneElement(child, {...props})
+            })}
+        </div>
+    )
+}
